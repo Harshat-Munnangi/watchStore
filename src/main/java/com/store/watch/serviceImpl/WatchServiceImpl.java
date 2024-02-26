52ed683682e02ac2a5ee5e1c553234428275466c
Harshat-Munnangi <munnangi.harshat@gmail.com>
2024-02-26 21:18:59 +0100

    add logging

--- src/main/java/com/store/watch/serviceImpl/WatchServiceImpl.java
+++ src/main/java/com/store/watch/serviceImpl/WatchServiceImpl.java
@@ -6,6 +6,8 @@ import com.store.watch.exception.CheckoutCalculationException;
 import com.store.watch.exception.EmptyWatchListException;
 import com.store.watch.exception.WatchNotFoundException;
 import com.store.watch.service.WatchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,8 @@ import java.util.stream.Collectors;
 @Service
 public class WatchServiceImpl implements WatchService {
 
+    private static final Logger log = LoggerFactory.getLogger(WatchServiceImpl.class);
+
     private final WatchDao watchDao;
 
     @Autowired
@@ -27,9 +31,13 @@ public class WatchServiceImpl implements WatchService {
 
     @Override
     public int checkoutWatches(List<String> watchIds) throws WatchNotFoundException {
+        log.info("Received request to checkout watches with IDs: {}", watchIds);
+
         if (watchIds.isEmpty()) {
+            log.warn("Received an empty list of watch IDs. No watches to checkout.");
             throw new EmptyWatchListException("No watches to checkout");
         }
+
         Map<String, Integer> watchIdCountMap = new HashMap<>();
         watchIds.stream()
                 .map(String::trim)
@@ -41,22 +49,26 @@ public class WatchServiceImpl implements WatchService {
                 .parallelStream()
                 .map(watch -> watch.calculateCost(watchIdCountMap.getOrDefault(watch.id(), 0)))
                 .reduce(Integer::sum)
-                .orElseThrow(() -> new CheckoutCalculationException("Unable to checkout watches"));
+                .orElseThrow(() -> {
+                    log.error("Unexpected error calculating total cost for checkout: {}", watchIds);
+                    return new CheckoutCalculationException("Unable to checkout watches");
+                });
     }
 
     private List<Watch> validateAndFetchWatchList(Set<String> watchIdCountMapKeys) {
         List<Watch> availableWatchList = watchDao.getWatchesList(watchIdCountMapKeys);
-        Set<String> availableWatchIds = availableWatchList.stream().map(Watch::id).collect(Collectors.toSet());
-        validateWatchIds(availableWatchIds, watchIdCountMapKeys);
+        validateWatchIds(availableWatchList, watchIdCountMapKeys);
         return availableWatchList;
     }
 
-    private void validateWatchIds(Set<String> availableWatchIds, Set<String> watchIdCountMapKeys) {
+    private void validateWatchIds(List<Watch> availableWatchList, Set<String> watchIdCountMapKeys) {
+        Set<String> availableWatchIds = availableWatchList.stream().map(Watch::id).collect(Collectors.toSet()); // converted due to better performance
         String invalidWatchIds = watchIdCountMapKeys.stream()
                 .filter(watchId -> !availableWatchIds.contains(watchId))
                 .collect(Collectors.joining(", "));
 
         if (!invalidWatchIds.isEmpty()) {
+            log.warn("Invalid watch IDs found: {}", invalidWatchIds);
             throw new WatchNotFoundException("Watch with ID/s (" + invalidWatchIds + ") not found");
         }
     }
